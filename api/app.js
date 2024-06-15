5b58e63cac9c485bfbb80b73a3a08d7914e1faa0
Suraj Bodke <surajbodke25@gmail.com>
2024-06-15 12:17:37 +0530

    Enhance functionality with API connections and dynamic profile updates

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8800, () => {
   console.log("server is running");
